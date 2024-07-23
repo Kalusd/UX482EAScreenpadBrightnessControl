7b68d5f7650031ca2de435f48e9e62d554365c78
Lukas Dizy <69392457+Kalusd@users.noreply.github.com>
2024-07-23 04:15:02 +0200

    Add files via upload

--- extension.js
+++ extension.js
@@ -9,11 +9,42 @@ class BrightnessIndicator extends PanelMenu.Button {
     _init() {
         super._init(0.0, 'Screenpad Brightness');
 
-        // Add a new menu item
-        this.brightnessMenuItem = new PopupMenu.PopupBaseMenuItem({ activate: false });
+        // Create a box to hold the icon and slider
+        let box = new St.BoxLayout({
+            vertical: false,
+            style_class: 'brightness-box',
+            x_expand: true,   // Allow the box to expand horizontally
+            y_expand: false   // Prevent the box from expanding vertically
+        });
+
+        // Create an icon
+        this.brightnessIcon = new St.Icon({
+            icon_name: 'display-brightness-symbolic', // Use a GNOME symbolic icon
+            style_class: 'system-status-icon',
+            icon_size: 16
+        });
+
+        // Create the slider
         this.brightnessSlider = new Slider.Slider(0.5);
         this.brightnessSlider.connect('notify::value', this._onSliderValueChanged.bind(this));
-        this.brightnessMenuItem.add(this.brightnessSlider);
+
+        // Ensure the slider occupies all available space
+        this.brightnessSlider.x_expand = true; // Allow the slider to expand horizontally
+
+        // Create and add a spacer between the icon and the slider
+        let spacer = new St.BoxLayout({ 
+            style_class: 'brightness-spacer',
+            width: 10, // Adjust as necessary
+        });
+
+        // Add the icon, spacer, and slider to the box
+        box.add_child(this.brightnessIcon);
+        box.add_child(spacer);
+        box.add_child(this.brightnessSlider);
+
+        // Add the box to the menu item
+        this.brightnessMenuItem = new PopupMenu.PopupBaseMenuItem({ activate: false });
+        this.brightnessMenuItem.add_child(box);
 
         // Insert the new brightness slider below the primary brightness slider
         Main.panel.statusArea.aggregateMenu.menu.addMenuItem(this.brightnessMenuItem, 2);
@@ -34,17 +65,9 @@ class BrightnessIndicator extends PanelMenu.Button {
     _onSliderValueChanged(slider) {
         let brightness = Math.round(slider.value * 245) + 10;
         log(`Screenpad Brightness Extension: Setting brightness to: ${brightness}`);
-
-        // Command to execute
-        let command = `python3 ~/.local/share/gnome-shell/extensions/screenpad-brightness@local/set_brightness.py ${brightness}`;
         
         // Execute the command
-        
-        // GLib.spawn_command_line_async(`echo ${brightness} | tee /sys/class/leds/asus::screenpad/brightness > /dev/null`);
-        let debug = GLib.spawn_command_line_async(`/bin/sh -c 'echo ${brightness} | tee /sys/class/leds/asus::screenpad/brightness'`);
-        
-        log(`debug=${debug}`);
-
+        GLib.spawn_command_line_async(`/bin/sh -c 'echo ${brightness} | tee /sys/class/leds/asus::screenpad/brightness'`);
     }
 });
 
@@ -65,3 +88,4 @@ function disable() {
     }
     log('Screenpad Brightness Extension disabled');
 }
+
